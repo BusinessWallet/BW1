c2492580bb61079f46371d18aaeaccfccc320c6a
Hissah <Hissah@MacBook-Pro-alkhas-b-hsh.local>
2016-02-10 22:24:36 +0300

    Keyboard

--- BusinessWallet/SecondViewController.swift
+++ BusinessWallet/SecondViewController.swift
@@ -10,6 +10,25 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    
+    // أضفت العناصر عشان أستخدمهم في إخفار لوحة المفاتيح في الدوال اللي تحتهم
+    @IBOutlet weak var EmailTextField: UITextField!
+    @IBOutlet weak var PasswordTextField: UITextField!
+    
+    
+    //دالتين ربطتها بال  buttons عشان تختفي لوحة المفاتيح لما نضغطهم
+    @IBAction func ForgetPasswordButton(sender: AnyObject) {
+        self.EmailTextField.resignFirstResponder()
+        self.PasswordTextField.resignFirstResponder()
+    }
+    
+    @IBAction func SignInButton(sender: AnyObject) {
+        self.EmailTextField.resignFirstResponder()
+        self.PasswordTextField.resignFirstResponder()
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +39,13 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    // دالة أضفتها عشان تختفي لوحة المفاتيح لما نلمس أي مكان على الشاشة
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    
+    
 }
 
